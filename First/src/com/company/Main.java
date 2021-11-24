7bc34b22e5714b7d218c31bde0b529dce2c5e946
Balakrishna1308 <91240572+Balakrishna1308@users.noreply.github.com>
2021-11-24 14:16:55 -0500

    Java projects done on IntelliJIDEA. It included folders

---
+++ First/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("First");
+        System.out.println("I am doing with my first coding");
+        System.out.println("Thank you");
+        System.out.println("We will see you tomorrow");
+    }
+}
